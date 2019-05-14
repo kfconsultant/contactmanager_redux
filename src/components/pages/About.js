656a7db606f7ba09c7359dca411e567ee8116927
kfconsultant <kfconsultant@gmail.com>
2019-05-14 17:32:39 -0600

    updated the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default class About extends Component {
     return (
       <div>
         <h1 className="display-6">About Contact Manager</h1>
-        <p className="lead">Simple app to manage contacts</p>
+        <p className="lead">Simple manage contacts app using React + Redux</p>
         <p className="text-secondary">1.0.0</p>
         <p>React Version: {React.version}</p>
         <p>Created by: Kelvin F (61920)</p>
